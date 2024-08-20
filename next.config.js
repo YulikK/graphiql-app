c1c32ecf9da691709ca860907548476aa15b0f07
YulikK <yulikpoch@gmail.com>
2024-08-20 12:33:02 +0200

    chore: add netlify toml

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/index.html',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
