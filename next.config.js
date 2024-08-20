305b3f5e8b1a78d1684cc8f1303eed1641c3bc55
YulikK <yulikpoch@gmail.com>
2024-08-20 15:22:30 +0200

    chore: next conf

--- next.config.js
+++ next.config.js
@@ -1,17 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  cleanUrls: true,
-  trailingSlash: true,
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        destination: '/index.html',
-        permanent: true,
-      },
-    ];
-  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/_next/' : '',
+  // cleanUrls: true,
+  // trailingSlash: true,
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/_next/' : '',
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/:path*',
+  //       destination: '/index.html',
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
