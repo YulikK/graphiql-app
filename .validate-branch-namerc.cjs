dc99bcd49be2b077834c418e74103d6223ff364a
YulikK <yulikpoch@gmail.com>
2024-08-20 11:46:30 +0200

    chore: update branch name template

--- .validate-branch-namerc.cjs
+++ .validate-branch-namerc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
   pattern:
-    /^develop|^(feat|fix|hotfix|chore|refactor|revert|docs|style|test|)\(#\d{3}\)\/[a-z]+[a-zA-Z0-9]*$/,
+    /^develop|^(feat|fix|hotfix|chore|refactor|revert|docs|style|test|)\(#\d+\)-[a-z]+[a-zA-Z0-9]*$/,
   errorMsg: 'Please use correct branch name',
 };
