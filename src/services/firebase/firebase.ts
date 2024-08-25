ee6528f0bbb951f84b0f4d1039ed73a71ceb0d03
Yuliya Karuk <karuk.yuliya@gmail.com>
2024-08-25 10:36:56 +0300

    refactor: add firebase consts in .env

--- src/services/firebase/firebase.ts
+++ src/services/firebase/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAQNilxD4GBZxTUhmoBNjyivqwUzMxtO3A',
-  authDomain: 'graphiql-app-6eb46.firebaseapp.com',
-  projectId: 'graphiql-app-6eb46',
-  storageBucket: 'graphiql-app-6eb46.appspot.com',
-  messagingSenderId: '420389553753',
-  appId: '1:420389553753:web:ea452289be80bd9dda9348',
-  measurementId: 'G-GTJZ3CYJW4',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
