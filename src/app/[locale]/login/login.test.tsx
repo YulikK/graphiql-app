c08eff57aa0f8d625d99a66259637f23c563654c
Yuliya Karuk <karuk.yuliya@gmail.com>
2024-09-11 17:56:39 +0300

    feat: add checks for input

--- src/app/[locale]/login/login.test.tsx
+++ src/app/[locale]/login/login.test.tsx
@@ -1,4 +1,5 @@
 import { cleanup, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import { vi } from 'vitest';
 
 import { renderWithProviders } from '@/shared/test-setup/render-router';
@@ -9,8 +10,29 @@ vi.mock('react-firebase-hooks/auth', () => ({
   useAuthState: () => [null, false, null],
 }));
 
+type Translations = {
+  [key: string]: string;
+};
+
+const translations: Translations = {
+  title: 'Login',
+  'email-input': 'Email',
+  'password-input': 'Password',
+  'sign-up-button': 'Sign In',
+  'registration-text': "Don't have an account?",
+  'registration-link': 'Sign Up',
+  'google-text': 'Continue with Google',
+  'authentication-loading': 'Wait, your authentication in progress',
+  'success-login-message': 'Congratulations, you have successfully logged in!',
+  'unexpected-error': 'An unexpected error occurred.',
+  'email-required': 'Email is a required field',
+  'email-invalid': 'Must be a valid email format',
+};
+
 vi.mock('next-intl', () => ({
-  useTranslations: () => (key: string) => key,
+  useTranslations: () => (key: string) => {
+    return translations[key] || key;
+  },
 }));
 
 vi.mock('next/navigation', () => ({
@@ -32,7 +54,23 @@ describe('Login Page', () => {
   it('it render inputs and button', async () => {
     renderWithProviders(<LoginPage params={{ locale: 'en' }} />);
 
-    expect(screen.getByTestId('email')).toBeInTheDocument();
+    const emailDiv = screen.getByTestId('email');
+    const emailInput = emailDiv.querySelector('input[name="email"]');
+    expect(emailInput).toBeInTheDocument();
+
+    await userEvent.type(emailInput!, 'testgmail.com');
+
+    const invalidError = await screen.findByText(translations['email-invalid']);
+    expect(invalidError).toBeInTheDocument();
+
+    await userEvent.clear(emailInput!);
+
+    const requiredError = await screen.findByText(
+      translations['email-required']
+    );
+
+    expect(requiredError).toBeInTheDocument();
+
     expect(screen.getByTestId('password')).toBeInTheDocument();
   });
 });
