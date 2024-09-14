c5cf25f8c5aa228aed2c852856b542d6bc34b174
YulikK <yulikpoch@gmail.com>
2024-09-14 11:12:00 +0200

    feat: check documentation disable button

--- src/widgets/docs/docs.tsx
+++ src/widgets/docs/docs.tsx
@@ -94,12 +94,12 @@ export const Docs = () => {
 
   return (
     <>
-      <Tooltip title={t(urlDoc ? 'show-doc' : 'need-doc-url')} arrow>
+      <Tooltip title={t(schema ? 'show-doc' : 'need-doc-url')} arrow>
         <span>
           <IconButton
             size="small"
             color="primary"
-            disabled={!urlDoc}
+            disabled={!schema}
             sx={{ p: '10px', ml: 'auto' }}
             aria-label={t('show-doc')}
             onClick={handleDocOpen}
